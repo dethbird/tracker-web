1b10892e75565558834bfb2c265eab5c8c7ec252
Rishi Satsangi <rishi.satsangi@gmail.com>
2014-06-08 08:32:00 -0400

    user auth

--- www/index.php
+++ www/index.php
@@ -20,7 +20,11 @@
 	$env = parse_ini_file("../env.ini");
 	$configs = parse_ini_file($env['config_file']);
 
-	global $api_url, $user_id, $client;
+	global $api_url, 
+		$user_id,
+		$client,
+		$googleClient;
+
 	$user_id = 1;
 	$api_url = $configs['api_url'];
 
@@ -37,10 +41,20 @@
 	require '../vendor/autoload.php';
 
 	use Guzzle\Http\Client;
+	use Google\Client as GoogleClient;
+
+	$googleClient = new Google_Client();
+	$googleClient->setClientId("478947664225-j1fndcve7sc45mia8rrqghvjd7u9j5df.apps.googleusercontent.com");
+	$googleClient->setClientSecret("F-YEjw-iV91SURUgp3w2g4WZ");
+	$googleClient->setScopes("https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/userinfo.email");
+	$googleClient->setRedirectUri("http://tracker-dev.rishisatsangi.com/callback/oauth/google");
+	
 
 	$client = new Client($api_url, array(
 	    "request.options" => array(
-	       "headers" => array("user_id" => $user_id)
+	       "headers" => array(
+		       "auth_token" => isset($_COOKIE['auth_token']) ? $_COOKIE['auth_token'] : false
+	       	)
 	    )
 	));
 
@@ -74,7 +88,7 @@
 	    public function activity_name_label($activity_type_id, $acivity_types)
 	    {
 	    	$activity_type = $acivity_types[$activity_type_id];
-	    	return '<span class="label label-'.($activity_type['polarity']>0?"success":"danger").'">'.$activity_type['name'].'</span>';
+	    	return '<span class="label label-'.($activity_type['polarity']>0?"suc cess":"danger").'">'.$activity_type['name'].'</span>';
 	    }
 
 	}
@@ -99,33 +113,81 @@
 	*         \/                           \//_____/  	
 	*/
 
-	$app->get('/', function () use ($app) {
+	/**
+	* Authentication should be run as middleware before each route
+	*/
+	$authCheck = function($app, $client) 
+	{
+		return function () use ( $app, $client ) 
+		{
+
+			//if no auth token
+			if (!isset($_COOKIE['auth_token'])) {
+    			$app->redirect('/auth');
+			}
+
+			//if user not in session
+			if (!isset($_SESSION['user'])) {
+
+				//try to get user from the auth token
+				$response = $client->get("/user/token-auth")->send();
+				$response = json_decode($response->getBody(true));
+
+				if($response->status===true && isset($response->data->id)) {
+					$_SESSION['user'] = $response->data;
+					setcookie("auth_token", $_SESSION['user']->auth_token, time()+60*60*24*30, "/", "rishisatsangi.com", false, true);
+					$_COOKIE['auth_token'] = $_SESSION['user']->auth_token;
+					$app->redirect('/');
+				} else {
+					//clear cookies and session
+					unset($_SESSION['user']);
+					setcookie("auth_token", null, time() - 3600);
+					unset($_COOKIE['auth_token']);
+					$app->redirect('/auth');
+				}
+			}
+			
+		};
+	};
+
+
+	$app->get('/', $authCheck($app, $client), function () use ($app) {
 	    $app->redirect('/activity/add');
 	});
 
 
-	/**
-	*  ACTIVITY
-	*/
-	$app->get('/activity/log', function () use ($app, $client) {
+	$app->get('/auth', function () use ($app, $googleClient) {
 
-		$response = $client->get("/activity/log")->send();
-		$response = json_decode($response->getBody(true));
-
-	    $app->render('partials/activity_log.html.twig', array(
+		$app->render('partials/auth.html.twig', array(
 	    	"section"=>$app->environment()->offsetGet("PATH_INFO"),
-	    	"activity"=>$response->data
+	    	"googleAuthUrl" => $googleClient->createAuthUrl()
     	));
 
 	});
 
-	//add new log entry form
-	$app->get('/activity/add', function () use ($app, $client) {
+	$app->get('/logout', function () use ($app, $googleClient) {
+		
+		//clear cookies and session
+		unset($_SESSION['user']);
+		setcookie("auth_token", "", time() - 3600, "/", "rishisatsangi.com", false, true);
+		unset($_COOKIE['auth_token']);
+		session_destroy();
+		$app->redirect('/auth');
 
+	});
+
+	/**
+	*  ACTIVITY
+	*/
+
+	//add new log entry form
+	$app->get('/activity/add', $authCheck($app, $client), function () use ($app, $client) {
+		// var_dump($client);die();
 		$typeResponse = json_decode($client->get("activity/type")->send()->getBody(true));
 	    $app->render('partials/activity_form.html.twig', array(
 	    	"section"=>$app->environment()->offsetGet("PATH_INFO"),
-	    	"types" => $typeResponse->data
+	    	"types" => $typeResponse->data,
+	    	"user" => $_SESSION['user']
     	));
 	});
 
@@ -164,7 +226,8 @@
 		// print_R($typeResponse);
 	    $app->render('partials/activity_type_list.html.twig', array(
 	    	"section"=>$app->environment()->offsetGet("PATH_INFO"),
-	    	"types" => $typeResponse->data
+	    	"types" => $typeResponse->data,
+	    	"user" => $_SESSION['user']
     	));
 	});
 
@@ -172,7 +235,8 @@
 	$app->get('/activity/type/add', function () use ($app, $client) {
 
 	    $app->render('partials/activity_type_form.html.twig', array(
-	    	"section"=>"/activity/type"
+	    	"section"=>"/activity/type",
+	    	"user" => $_SESSION['user']
     	));
 	});
 
@@ -217,7 +281,8 @@
 
 	    $app->render('partials/activity_type_form.html.twig', array(
 	    	"section"=>"/activity/type",
-	    	"type" => $response->data[0]
+	    	"type" => $response->data[0],
+	    	"user" => $_SESSION['user']
     	));
 	});
 
@@ -239,8 +304,57 @@
 
 	    $app->render('partials/activity_report_by_day.html.twig', array(
 	    	"section"=>$app->environment()->offsetGet("PATH_INFO"),
-	    	"report"=>$response['data']
+	    	"report"=>$response['data'],
+	    	"user" => $_SESSION['user']
     	));
+
+	});
+
+	/**
+	*  OAUTH CALLBACK 
+	*/
+
+	$app->get('/callback/oauth/google', function () use ($app, $client, $googleClient) {
+
+		$googleClient->authenticate($_GET['code']);
+		$accessToken = $googleClient->getAccessToken();
+		$googleClient->setAccessToken($accessToken);
+
+		$plus = new \Google_Service_Plus($googleClient);
+		$person = $plus->people->get('me');
+
+		$emails = $person->getEmails();
+		$email = $emails[0]->value;
+
+		$params = array(
+			"name"=>$person->getDisplayName(),
+			"email"=>$email
+		);
+
+		$client->setDefaultOption(
+	       "headers",  array(
+		       "auth_token" => "c4ca4238a0b923820dcc509a6f75849b"
+	       	)
+	    );
+
+		$request = $client->post("/user", array(), $params);
+		$response = $request->send();
+		$response = json_decode($response->getBody(true));
+
+		// echo "<pre>"; print_r($response); echo "</pre>"; die();
+
+		$_SESSION['user'] = $response->data;
+		setcookie("auth_token", $_SESSION['user']->auth_token, time()+60*60*24*30, "/", "rishisatsangi.com", false, true);
+		$_COOKIE['auth_token'] = $_SESSION['user']->auth_token;
+
+		// add flash message
+		if(isset($_SESSION['user']->is_new)){
+			$app->flash("success", "Account created");
+		} else {
+			$app->flash("success", "Logged in");
+		}
+
+		$app->redirect("/");
 
 	});
 
